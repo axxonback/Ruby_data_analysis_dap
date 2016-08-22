f30fef26791df87c2f6790f5b2ea0a82c062d2be
Jon Hart <jon_hart@rapid7.com>
2016-08-22 09:06:29 -0700

    Beginnings of unit tests for Dap::Filter::FilterDecodeHTTPReply

---
+++ spec/dap/filter/http_filter_spec.rb
@@ -0,0 +1,45 @@
+require 'zlib'
+
+describe Dap::Filter::FilterDecodeHTTPReply do
+  describe '.decode' do
+
+    let(:filter) { described_class.new(['data']) }
+
+
+    context 'decoding non-HTTP response' do
+      let(:decode) { filter.decode("This\r\nis\r\nnot\r\nHTTP\r\n\r\n") }
+      it 'returns an empty hash' do
+        expect(decode).to eq({})
+      end
+    end
+
+    context 'decoding uncompressed response' do
+      let(:decode) { filter.decode("HTTP/1.0 200 OK\r\nHeader1: value1\r\n\r\nstuff") }
+
+      it 'correctly sets status code' do
+        expect(decode['http_code']).to eq(200)
+      end
+
+      it 'correctly sets status message' do
+        expect(decode['http_message']).to eq('OK')
+      end
+
+      it 'correctly sets body' do
+        expect(decode['http_body']).to eq('stuff')
+      end
+
+      it 'correct extracts header(s)' do
+        expect(decode['http_raw_headers']).to eq({'header1' => 'value1'})
+      end
+    end
+
+    context 'decoding compressed response' do
+      let(:decode) { filter.decode("HTTP/1.0 200 OK\r\nContent-encoding: gzip\r\n\r\n#{Zlib::Deflate.deflate('stuff')}") }
+
+      it 'correctly sets body' do
+        expect(decode['http_body']).to eq('stuff')
+      end
+    end
+
+  end
+end
